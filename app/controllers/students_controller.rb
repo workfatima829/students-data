8b1e7c697798b2799ab3820132f1638504b2d8a2
workfatima829 <workfatima829@gmail.com>
2026-01-13 17:14:04 +0500

    Now the student delete successfully

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,49 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: %i[show edit update destroy]
+
   def index
+    @students = Student.all
   end
 
   def show
   end
 
   def new
+    @student = Student.new
   end
 
   def edit
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to @student, notice: "Student was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @student.update(student_params)
+      redirect_to @student, notice: "Student was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @student.destroy
+    redirect_to students_url, notice: "Student was successfully deleted."
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :age, :email)
   end
 end
